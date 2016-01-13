3b2cbbc33d08bc96f772804e8d6b65f5f9d41ede
Nandor Elod Fekete <nandor.fekete@innometa.com>
2016-01-13 03:48:14 +0100

    fix for http://stackoverflow.com/questions/34160584/how-to-have-separate-authentication-sources-one-for-oauth2-and-one-for-form-ba

--- src/main/java/mx/sisu/tokenenhancertest/config/WebSecurityConfig.java
+++ src/main/java/mx/sisu/tokenenhancertest/config/WebSecurityConfig.java
@@ -1,41 +1,41 @@
 package mx.sisu.tokenenhancertest.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig{
+public class WebSecurityConfig {
 	
 	@Configuration
-	@Order(5)
+	@Order(-11)
 	public static class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			
+			System.out.println("AdminSecurityConfig LOADED!!!");
 			http.csrf().disable();
 			http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/admin_logout"))
 					.invalidateHttpSession(true).logoutSuccessUrl("/admin/login.html");
 
-			http.authorizeRequests()
-					.antMatchers("/admin/login.html").permitAll().antMatchers("/admin/protected.html")
-					.hasRole("ADMIN")
-					.and().formLogin().loginPage("/admin/login.html")
-					.loginProcessingUrl("/admin_login").defaultSuccessUrl("/admin/protected.html");
+		http.requestMatchers().antMatchers("/admin/**")
+			.and()
+				.authorizeRequests()
+				.antMatchers("/admin/protected.html").hasRole("ADMIN")
+				.antMatchers("/admin/login.html").permitAll()
+			.and()
+				.formLogin().loginPage("/admin/login.html")
+				.loginProcessingUrl("/admin_login")
+				.defaultSuccessUrl("/admin/protected.html")
+				;
 
 		}
 		
@@ -44,7 +44,9 @@ public class WebSecurityConfig{
 			//Data source for form based auth
 			auth.inMemoryAuthentication().withUser("adminuser").password("adminpassword").roles("ADMIN");
 		}
+
 	}
+	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		//Data source for Oauth
